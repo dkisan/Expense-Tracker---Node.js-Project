c601212c3cf75187e44b1d9f37a6380d7a572930
DebasishKisan1 <debasish.kisan@gmail.com>
2023-12-04 16:35:27 +0530

    Creating Login Screen Frontend with backend api

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -6,6 +6,10 @@ exports.gethome = (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'view', '/signup.html'))
 }
 
+exports.getlogin = (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'view', '/login.html'))
+}
+
 exports.postsignup = async (req, res, next) => {
     try {
         const user = await User.create({
@@ -16,6 +20,29 @@ exports.postsignup = async (req, res, next) => {
         res.send(user)
     }
     catch (err) {
+        res.send(err)
+    }
+}
+
+exports.postlogin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        })
+        if (user) {
+            if (req.body.password == user.password) {
+                res.status(200).send({ message: "User Login Successfully" })
+            } else {
+                res.status(401).send({ message: "Incorrect Credentials" })
+            }
+        } else {
+            res.status(404).send({ message: "User Not Found" })
+        }
+    }
+    catch (err) {
+        console.log('here')
         res.send(err)
     }
 }
--- routes/userroute.js
+++ routes/userroute.js
@@ -4,7 +4,10 @@ const router = express.Router()
 const usercontroller = require('../controller/usercontroller')
 
 router.get('/',usercontroller.gethome)
+router.get('/login',usercontroller.getlogin)
 
 router.post('/user/signup',usercontroller.postsignup)
+router.post('/user/login',usercontroller.postlogin)
+
 
 module.exports = router;
