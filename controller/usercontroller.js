82b74743e99f9828e97afd8b09fae0f85145549b
DebasishKisan1 <debasish.kisan@gmail.com>
2023-12-04 19:58:35 +0530

    bcrypt password

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+
 const User = require('../model/user')
 
 exports.gethome = (req, res, next) => {
@@ -12,12 +16,18 @@ exports.getlogin = (req, res, next) => {
 
 exports.postsignup = async (req, res, next) => {
     try {
-        const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        let password = req.body.password;
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+
+                const user = await User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash
+                })
+                res.send(user)
+            })
         })
-        res.send(user)
     }
     catch (err) {
         res.send(err)
@@ -32,17 +42,18 @@ exports.postlogin = async (req, res, next) => {
             }
         })
         if (user) {
-            if (req.body.password == user.password) {
-                res.status(200).send({ message: "User Login Successfully" })
-            } else {
-                res.status(401).send({ message: "Incorrect Credentials" })
-            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (result) {
+                    res.status(200).send({ message: "User Login Successfully" })
+                } else {
+                    res.status(401).send({ message: "Incorrect Credentials" })
+                }
+            })
         } else {
             res.status(404).send({ message: "User Not Found" })
         }
     }
     catch (err) {
-        console.log('here')
         res.send(err)
     }
 }
